109bf72bc0c7a1713a1cc2ee0dba3b75ec269888
David Aerne <meodai@gmail.com>
2022-01-09 00:55:20 +0100

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+
+const pages = [
+  'https://en.wikipedia.org/wiki/List_of_colors:_A%E2%80%93F',
+  'https://en.wikipedia.org/wiki/List_of_colors:_G%E2%80%93M',
+  'https://en.wikipedia.org/wiki/List_of_colors:_N%E2%80%93Z',
+]
+
+let colors = [];
+
+(async () => {
+  const browser = await puppeteer.launch();
+  
+  for (let i = 0; i < pages.length; i++) {
+    const page = await browser.newPage();
+    await page.goto(pages[i]);
+    const colorList = await page.evaluate(() => {
+      const colorList = [];
+      const colorTable = document.querySelector('table.wikitable');
+      const colorRows = colorTable.querySelectorAll('tr');
+      for (let i = 1; i < colorRows.length; i++) {
+        const colorRow = colorRows[i];
+        const colorName = colorRow.querySelector('th a').innerText;
+        const colorHex = colorRow.querySelector('td:nth-child(2)').innerText;
+        colorList.push({
+          name: colorName,
+          hex: colorHex.toLowerCase()
+        });
+      }
+      return colorList;
+    });
+    colors = colors.concat(colorList);
+  }
+
+  await browser.close();
+  fs.writeFileSync('./colors.json', JSON.stringify(colors));
+})();
