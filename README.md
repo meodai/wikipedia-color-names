13b78b168a019547d91aae5f11d3edc677a73eb0
David Aerne <meodai@gmail.com>
2022-01-09 22:00:05 +0100

    adds color names to readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # wikipedia-color-names
 Up to date list of wikipedia color names
+## Latest Color Names 🔖
+![Wikipedia Color Names](colors.svg "List of wikipedia colors")
---
+++ scripts/createimage.js
@@ -0,0 +1,26 @@
+const colorList = require('../colors.json');
+const fs = require('fs');
+const path = require('path');
+
+// gets SVG template
+const svgTpl = fs.readFileSync(
+  path.normalize(__dirname + '/template.svg.tpl'),
+  'utf8'
+).toString();
+
+// generates an SVG image with the new color based on the diff ot the last commit to the current
+function diffSVG() {
+
+  const svgTxtStr = colorList.reduce((str, c, i) => {
+    return `${str}<text x="40" y="${20 + (i + 1) * 70}" fill="${c.hex}">${c.name.replace(/&/g, '&amp;')}</text>`;
+  }, '');
+
+  fs.writeFileSync(
+    path.normalize(`./colors.svg`),
+    svgTpl.replace(/{height}/g, colorList.length * 70 + 80)
+    .replace(/{items}/g, svgTxtStr)
+  );
+
+};
+
+diffSVG();
