6f1c5ae9e1533bca0e33ccc7073191bc37ef137e
David Aerne <meodai@gmail.com>
2022-09-21 21:14:22 +0200

    feat(html): adds HTML export

--- scripts/createimage.js
+++ scripts/createimage.js
@@ -2,9 +2,14 @@ const colorList = require('../colors.json');
 const fs = require('fs');
 const path = require('path');
 
-// gets SVG template
+// get SVG template
 const svgTpl = fs.readFileSync(
   path.normalize(__dirname + '/template.svg.tpl'),
+  'utf8'
+).toString();
+
+const htmlTpl = fs.readFileSync(
+  path.normalize(__dirname + '/template.html.tpl'),
   'utf8'
 ).toString();
 
@@ -14,10 +19,19 @@ function diffSVG() {
     return `${str}<text x="40" y="${20 + (i + 1) * 70}" fill="${c.hex}">${c.name.replace(/&/g, '&amp;')}</text>`;
   }, '');
 
+  const htmlColorEntries = colorList.reduce((str, c, i) => {
+    return `${str}<div><a href="${c.link}" style="color: ${c.hex};">${c.name}</a></div>`;
+  }, '');
+
   fs.writeFileSync(
     path.normalize(`./colors.svg`),
     svgTpl.replace(/{height}/g, colorList.length * 70 + 80)
     .replace(/{items}/g, svgTxtStr)
+  );
+
+  fs.writeFileSync(
+    path.normalize(`./colors.html`),
+    htmlTpl.replace(/{items}/g, htmlColorEntries)
   );
 };
 
---
+++ scripts/template.html.tpl
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>List of Wikipedia Color-Names</title>
+    <style>
+      @import url('https://rsms.me/inter/inter.css');
+      body * {
+        all: unset;
+      }
+      :root {
+        font-family: Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        font-weight: 900;
+        font-size: 140%;
+        background: #202124;
+      }
+      div, a {
+        display: block;
+      }
+      div {
+        margin: 0.5em;
+      }
+      a {
+        cursor: pointer;
+      }
+      a:hover {
+        text-decoration: underline;
+      }
+    </style>
+  </head>
+  <body>
+  {items}
+  </body>
+</html>
